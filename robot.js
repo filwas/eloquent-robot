9bf8f91df6b9c40915ba73840cf167528ef6ab58
Filip Was <fwas.corp@gmail.com>
2023-05-02 18:59:13 +0200

    first function that i wrote myself for this exrcs

--- robot.js
+++ robot.js
@@ -49,7 +49,6 @@ class VillageState{
 }
 
 function runRobot(state, robot, memory) {
-    console.log(state.parcels);
     for (let turn = 0;; turn++) {
         if(state.parcels.length == 0){
             console.log(`Done in ${turn} turns`);
@@ -86,25 +85,25 @@ VillageState.random = function(parcelCount = 5) {
 
 let randomState = VillageState.random();
 
-runRobot(randomState, randomRobot)
+//runRobot(randomState, randomRobot)
 
 var mailRoute = [
     "Alice's House", "Cabin", "Alice's House", "Bob's House",
     "Town Hall", "Daria's House", "Ernie's House",
     "Grete's House", "Shop", "Grete's House", "Farm",
     "Marketplace", "Post Office"
-  ];
+];
 
-  function routeRobot(state, memory = []) {
+function routeRobot(state, memory = []) {
     if (memory.length == 0) {
         memory = mailRoute;
     }
     return {direction: memory[0], memory: memory.slice(1)};
-  }
+}
   
-  runRobot(randomState, routeRobot)
+//runRobot(randomState, routeRobot)
 
-  function findRoute(graph, from, to) {
+function findRoute(graph, from, to) {
     let work = [{at: from, route: []}];
     for (let i = 0; i < work.length; i++) {
       let {at, route} = work[i];
@@ -115,7 +114,7 @@ var mailRoute = [
         }
       }
     }
-  }
+}
 
 
 function goalOrientedRobot({place, parcels}, route = []) {
@@ -128,6 +127,49 @@ function goalOrientedRobot({place, parcels}, route = []) {
       }
     }
     return {direction: route[0], memory: route.slice(1)};
-  }
+}
 
-  runRobot(randomState, goalOrientedRobot)
+//runRobot(randomState, goalOrientedRobot)
+
+
+/*  
+    This is the first function I've created for this exercise myself. The task is to generate 100 different scenarios,
+    have two robots solve all of them and compare the average amount of steps it took to complete it. What I've
+    noticed is that basicaly all I needed to do is to make the function runRobot return the steps number at the end,
+    but I didn't want to change the original function. Therefore I've copy-pasted the original function, changed it's
+    name and made it do what I want it to do. 
+    The rest of the function is pretty straightforward. It generates an array and fills it with a hundred random states,
+    and then calls the counting function for both robots for each state. Thanks to the fact that robots don't change
+    the original states and create new ones as they go along, I don't need to make a deep copy of each starting state
+    and can feed both robots same thing. It simply adds up all of the steps needed to complete all 100 starting states
+    and divides it by 100 at the output.
+*/
+function compareRobots(robot1, memory1, robot2, memory2) {
+    let states = []
+    let robot1Moves = 0;
+    let robot2Moves = 0;
+    for (let i = 0; i < 100; i++) {
+        states.push(VillageState.random());
+    }    
+
+
+    states.forEach(currentState => {
+        robot1Moves += countRobot(currentState, robot1, memory1);
+        robot2Moves += countRobot(currentState, robot2, memory2);
+    });
+
+    console.log(`Robot 1 finished in ${robot1Moves/100}, robot 2 in ${robot2Moves/100}`);
+}
+
+function countRobot(state, robot, memory) {
+    for (let turn = 0;; turn++) {
+        if(state.parcels.length == 0){
+            return turn;
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+    }
+}
+  
+compareRobots(routeRobot, [], goalOrientedRobot, []);
